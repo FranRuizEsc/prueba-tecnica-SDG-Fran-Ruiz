09081d1325638f97279ed6f0a848b0a917f2deb5
FranRuizEsc <60076220+FranRuizEsc@users.noreply.github.com>
2025-03-25 10:55:12 +0100

    feat(SDG-1): Create main component, cuntries service and routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const ROUTES: Routes = [
+	{
+		path: '',
+		loadComponent: () => import('./components/main-page/main-page.component').then(m => m.MainPageComponent)
+	}
+];
