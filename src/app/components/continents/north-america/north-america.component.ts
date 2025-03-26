1a3dc109a44e332a02427d56dceb57509dbc20f1
FranRuizEsc <60076220+FranRuizEsc@users.noreply.github.com>
2025-03-26 10:37:24 +0100

    feat(SDG-2): Create and navigate to continent componet

---
+++ src/app/components/continents/north-america/north-america.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-north-america',
+  imports: [],
+  templateUrl: './north-america.component.html',
+  styleUrl: './north-america.component.scss',
+})
+export class NorthAmericaComponent {}
---
+++ src/app/components/continents/south-america/south-america.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-south-america',
+  imports: [],
+  templateUrl: './south-america.component.html',
+  styleUrl: './south-america.component.scss',
+})
+export class SouthAmericaComponent {}
