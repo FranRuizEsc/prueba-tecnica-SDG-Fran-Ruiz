1095412a66080ea9e95cecaac667d9fbe9f531d7
FranRuizEsc <60076220+FranRuizEsc@users.noreply.github.com>
2025-03-28 13:48:39 +0100

    feat(SDG-5): Display south america data

--- src/app/components/continents/south-america/south-america.component.ts
+++ src/app/components/continents/south-america/south-america.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { ContinentDetailsComponent } from '../../../shared/components/continent-details/continent-details.component';
 
 @Component({
   selector: 'app-south-america',
-  imports: [],
+  imports: [ContinentDetailsComponent],
   templateUrl: './south-america.component.html',
   styleUrl: './south-america.component.scss',
 })
-export class SouthAmericaComponent {}
+export class SouthAmericaComponent {
+  protected titlePopulation = 'Population of South American Countries';
+  protected continent = 'South America';
+}
