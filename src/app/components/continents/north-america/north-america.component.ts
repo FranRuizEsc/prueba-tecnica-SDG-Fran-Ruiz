dcf09aee691e087f1040659c14fe2a471217e8a6
FranRuizEsc <60076220+FranRuizEsc@users.noreply.github.com>
2025-03-28 13:46:47 +0100

    feat(SDG-4): Display north american data

--- src/app/components/continents/north-america/north-america.component.ts
+++ src/app/components/continents/north-america/north-america.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { ContinentDetailsComponent } from '../../../shared/components/continent-details/continent-details.component';
 
 @Component({
   selector: 'app-north-america',
-  imports: [],
+  imports: [ContinentDetailsComponent],
   templateUrl: './north-america.component.html',
   styleUrl: './north-america.component.scss',
 })
-export class NorthAmericaComponent {}
+export class NorthAmericaComponent {
+  protected titlePopulation = 'Population of North American Countries';
+  protected continent = 'North America';
+}
