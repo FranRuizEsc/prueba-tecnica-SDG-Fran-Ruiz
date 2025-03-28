dbf1cdec5741c4da0b36f59490632cde316aadb6
FranRuizEsc <60076220+FranRuizEsc@users.noreply.github.com>
2025-03-28 13:42:47 +0100

    feat(SDG-4): Display data in Antarctica

--- src/app/components/continents/antarctica/antarctica.component.ts
+++ src/app/components/continents/antarctica/antarctica.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { ContinentDetailsComponent } from '../../../shared/components/continent-details/continent-details.component';
 
 @Component({
   selector: 'app-antarctica',
-  imports: [],
+  imports: [ContinentDetailsComponent],
   templateUrl: './antarctica.component.html',
   styleUrl: './antarctica.component.scss',
 })
-export class AntarcticaComponent {}
+export class AntarcticaComponent {
+  protected titlePopulation = 'Population of Antarctic countries';
+  protected continent = 'Antarctic';
+}
